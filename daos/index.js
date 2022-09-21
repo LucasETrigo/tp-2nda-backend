914cf62190e2d44886ff72b9f17fd039a941d5bf
Lucas Trigo <lucasetrigo@gmail.com>
2022-09-21 13:06:23 -0300

    First Commit

---
+++ daos/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+
+const CarritoDaoArchivo = require("./carrito/CarritoDaoArchivo.js");
+const CarritoDaoFirebase = require("./carrito/CarritoDaoFirebase.js");
+const CarritoDaoMongoDB = require("./carrito/CarritoDaoMongoDB.js");
+
+const ProductosDaoArchivo = require("./productos/ProductosDaoArchivo.js");
+const ProductosDaoMongoDB = require("./productos/ProductosDaoMongoDB.js");
+const ProductosDaoFirebase = require("./productos/ProductosDaoFirebase.js");
+
+// Exportamos segun la variable DAO del .env
+
+if (process.env.DAO === "fs") {
+	exports.Carrito = CarritoDaoArchivo
+	exports.Productos = ProductosDaoArchivo
+} else if (process.env.DAO === "mongo") {
+	exports.Carrito = CarritoDaoMongoDB
+	exports.Productos = ProductosDaoMongoDB
+} else if (process.env.DAO === "firebase") {
+	exports.Carrito = CarritoDaoFirebase
+	exports.Productos = ProductosDaoFirebase
+} else {
+	console.log("Error al elegir sistema de datos")
+}
